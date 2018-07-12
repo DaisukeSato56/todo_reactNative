11d036b3a0ac6942f6d7f3379fb6d047ba52c054
佐藤大介 <workshift56@gmail.com>
2018-07-12 17:15:12 +0900

    todo一覧を作る機能を追加

--- App.js
+++ App.js
@@ -13,26 +13,54 @@ import {
   Text, 
   View,
   TextInput,
+  TouchableOpacity,
+  ScrollView,
 } from 'react-native';
 
 type Props = {};
 export default class App extends Component<Props> {
   state = {
     newTodo: '',
+    todos: [],
   }
 
   onChangeText(newTodo) {
-    this.setState({ newTodo })
+    this.setState({ newTodo });
+  }
+
+  onPressAdd() {
+    const { newTodo } = this.state;
+    this.setState({
+      newTodo: "",
+      todos: [newTodo, ...this.state.todos],
+    })
   }
 
   render() {
-    console.log(this.state);
+    console.log(this.state)
     return (
       <View style={styles.container}>
-        <TextInput 
+        <TextInput
+          value={this.state.newTodo}
           style={styles.form}
           onChangeText={text => this.onChangeText(text)} 
         />
+        <TouchableOpacity 
+          style={styles.addButton}
+          onPress={() => this.onPressAdd()}
+        >
+          <Text style={styles.addButtonText}>ADD</Text>
+        </TouchableOpacity>
+
+        <ScrollView style={styles.scrollView}>
+          {
+            this.state.todos.map((todo, index) => (
+              <View key={todo+index} style={styles.todoContainer}>
+                <Text>{todo}</Text>
+              </View>
+            ))
+          }
+        </ScrollView>
       </View>
     );
   }
@@ -47,4 +75,22 @@ const styles = StyleSheet.create({
     backgroundColor: "#EEE",
     padding: 10,
   },
+  addButton: {
+    backgroundColor: "#333",
+    padding: 14,
+    borderRadius: 4,
+    marginTop: 10,
+  },
+  addButtonText: {
+    color: "#FFF",
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+  scrollView: {
+    backgroundColor: "#DDD"
+  },
+  todoContainer: {
+    backgroundColor: "#FFF",
+    padding: 10,
+  }
 });
