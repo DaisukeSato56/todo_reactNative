e6ea269d1131dbbdfd28bfc124f0e4c181653171
佐藤大介 <workshift56@gmail.com>
2018-07-12 16:49:08 +0900

    Initial commit

---
+++ App.js
@@ -0,0 +1,50 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {
+  Platform, 
+  StyleSheet, 
+  Text, 
+  View,
+  TextInput,
+} from 'react-native';
+
+type Props = {};
+export default class App extends Component<Props> {
+  state = {
+    newTodo: '',
+  }
+
+  onChangeText(newTodo) {
+    this.setState({ newTodo })
+  }
+
+  render() {
+    console.log(this.state);
+    return (
+      <View style={styles.container}>
+        <TextInput 
+          style={styles.form}
+          onChangeText={text => this.onChangeText(text)} 
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 30,
+  },
+  form: {
+    backgroundColor: "#EEE",
+    padding: 10,
+  },
+});
